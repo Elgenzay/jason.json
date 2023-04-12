d6dbe8829027d6a57e048209b7af2f7424725a28
Jason <jason@elg.gg>
2023-04-11 22:04:56 -0400

    Various changes

--- web/src/main.rs
+++ web/src/main.rs
@@ -11,83 +11,83 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 struct Resume {
-    pages: Vec<Page>,
-    header: Header,
+	pages: Vec<Page>,
+	header: Header,
 }
 
 #[derive(Serialize, Deserialize)]
 struct Page {
-    sidebar: Vec<SidebarItem>,
-    content: Vec<Content>,
+	sidebar: Vec<SidebarItem>,
+	content: Vec<Content>,
 }
 
 #[derive(Serialize, Deserialize)]
 struct Header {
-    name: String,
-    title: String,
-    contact: Contact,
-    anchor: String,
+	name: String,
+	title: String,
+	contact: Contact,
+	anchor: String,
 }
 
 #[derive(Serialize, Deserialize)]
 struct Contact {
-    website: String,
-    email: String,
-    phone: String,
-    location: String,
+	website: String,
+	email: String,
+	phone: String,
+	location: String,
 }
 
 #[derive(Serialize, Deserialize)]
 struct SidebarItem {
-    label: String,
-    items: Vec<String>,
+	label: String,
+	items: Vec<String>,
 }
 
 #[derive(Serialize, Deserialize)]
 struct Content {
-    label: String,
-    items: Vec<ContentItem>,
+	label: String,
+	items: Vec<ContentItem>,
 }
 
 #[derive(Serialize, Deserialize)]
 struct ContentItem {
-    title: String,
-    subtitle: String,
-    timeframe: String,
-    bullets: Vec<String>,
+	title: String,
+	subtitle: String,
+	timeframe: String,
+	bullets: Vec<String>,
 }
 
 #[rocket::get("/<path..>")]
 pub async fn static_pages(path: PathBuf) -> Option<NamedFile> {
-    let mut path = Path::new(relative!("static")).join(path);
-    if path.is_dir() {
-        path.push("index.html");
-    }
-    NamedFile::open(path).await.ok()
+	let mut path = Path::new(relative!("static")).join(path);
+	if path.is_dir() {
+		path.push("index.html");
+	}
+	NamedFile::open(path).await.ok()
 }
 
 #[rocket::get("/")]
 pub fn page() -> Template {
-    let filename = std::env::var("FILE_NAME").expect("Missing environment variable: FILE_NAME");
-    let jsondata =
-        std::fs::read_to_string(format!("json/{}", filename)).expect("Unable to read JSON file");
-    let resume: Resume = serde_json::from_str(&jsondata).expect("Unable to parse JSON file");
-    let mut context = HashMap::new();
-    context.insert("data", &resume);
-    Template::render("template", context)
+	let filename = std::env::var("FILE_NAME").expect("Missing environment variable: FILE_NAME");
+	let jsondata = std::fs::read_to_string(format!("static/data/{}", filename))
+		.expect("Unable to read JSON file");
+	let resume: Resume = serde_json::from_str(&jsondata).expect("Unable to parse JSON file");
+	let mut context = HashMap::new();
+	context.insert("data", &resume);
+	Template::render("template", context)
 }
 
 #[rocket::launch]
 fn rocket() -> _ {
-    dotenvy::dotenv().ok();
-    rocket::build()
-        .mount("/", rocket::routes![page, static_pages])
-        .register("/", rocket::catchers![not_found])
-        .attach(Template::fairing())
-        .attach(Shield::default().enable(Hsts::IncludeSubDomains(Duration::new(31536000, 0))))
+	dotenvy::dotenv().ok();
+	rocket::build()
+		.mount("/", rocket::routes![page, static_pages])
+		.register("/", rocket::catchers![not_found])
+		.attach(Template::fairing())
+		.attach(Shield::default().enable(Hsts::IncludeSubDomains(Duration::new(31536000, 0))))
 }
 
 #[catch(404)]
 pub fn not_found() -> Redirect {
-    Redirect::to("/")
+	Redirect::to("/")
 }
